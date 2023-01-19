9c581d6d935fbbf32218f89a33d0b9a8393c2ad0
maks-sova <maxxxhill2k@gmail.com>
2023-01-19 03:51:54 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Profile from "./Profile/Profile";
 import FriendList from "./FriendList/FriendList";
 import Statistics from "./Statistics/Statistics";
 import TransactionHistory from "./TransactionHistory/TransactionHistory";
+
 import user from "./folaund-json/user.json";
 import friends from "./folaund-json/friends.json";
 import data from "./folaund-json/data.json";
@@ -12,7 +13,8 @@ import transactions from "./folaund-json/transactions.json";
 export const App = () => {
   return (
   <div>
-  <>
+      <>
+       
       <Profile
   username={user.username}
   tag={user.tag}
@@ -33,4 +35,5 @@ export const App = () => {
       </div>
   );
 };
+
       
